e2eb3e8bb91bd08099135bd6eeea08e548d3f630
Abhinav Anand <abhinav.a@mantralabsglobal.com>
2023-07-19 10:19:32 +0530

    add assets

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ let PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('public'));
  
 app.post("/contact", addContact);
 app.get("/contact", getContact);
